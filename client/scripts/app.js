010439623428509fa8c94b8a30655bd3537f9fd1
Sherry Hsu <sherryh@oh-sherryh-mbp-2.local>
2017-01-02 16:43:07 -0800

    Completed Chatroom Behaviour tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,13 +1,15 @@
 // YOUR CODE HERE:
 
 var app = {
+  server: 'https://api.parse.com/',
+
   init: function() {
 
   },
   send: function(message) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: 'https://api.parse.com/1/classes/messages',
+      url: app.server + '/1/classes/messages',
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
@@ -22,21 +24,22 @@ var app = {
   },
   fetch: function() {
     $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-      url: 'https://api.parse.com/1/classes/messages',
+      url: app.server,
       type: 'GET',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message sent');
-      },
-      error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', data);
-      }
     });
+  },
 
-  }
+  clearMessages: function() {
+    $('#chats').empty();
+  },
 
+  renderMessage: function(message) {
+    $('#chats').append('<div>' + message + '</div>');
+  },
+
+  renderRoom: function(roomName) {
+    $('#roomSelect').append('<option value="'+roomName+'">' + roomName + '</option>');
+
+  },
 };
 
