03435711817613ce09858fe80e8f29c1a8885b10
Sherry Hsu <sherryh@oh-sherryh-mbp-2.local>
2017-01-03 16:08:47 -0800

    Able to display messages from server * messages formatted * text escaped * rooms populated in the dropdown

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,58 +1,115 @@
 // YOUR CODE HERE:
 
 var app = {
-  server: 'https://api.parse.com/',
+  server: 'https://api.parse.com/1/classes/messages',
 
-  init: function() {
-    $(document).ready(function() {
+  init: () => {
+    $(document).ready(() => {
+      var that = this;
+      // setInterval(() => {
+      //   that.fetch('');
+      // },6000);
+      app.fetch();
       $('#main').find('.username').click(app.handleUsernameClick);
-      $('#send .submit').submit(app.handleSubmit);
+      $('#send .submit').on('click', () => {
+        app.handleSubmit();
+      });
+      $('#roomSelect').change(()=>{
+        var $that = $(this);
+        //console.log($('option:selected').text());
+        app.fetch($('option:selected').val());
+      });
+
+      // $('#send .submit').submit(app.handleSubmit);
     });
   },
-  send: function(message) {
+  send: message => {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: app.server + '/1/classes/messages',
+      url: app.server,
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
-      success: function (data) {
+      success: data => {
         console.log('chatterbox: Message sent');
       },
-      error: function (data) {
+      error: data => {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message', data);
       }
     });
   },
-  fetch: function() {
+  fetch: (selectedRoomname = '') => {
     $.ajax({
       url: app.server,
       type: 'GET',
+      contentType: 'json',
+
+      success: data => {
+        console.log('chatterbox: received');
+        _.each(data.results, item => {
+          // app.renderMessage(item);
+          console.log('selectedRoomname:', selectedRoomname);
+          app.filterMessageByRoom(selectedRoomname, item);
+          app.renderRoom(item);
+        });
+      },
+      error: data => {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to load message', data);
+      }
     });
+
   },
 
-  clearMessages: function() {
+  clearMessages: () => {
     $('#chats').empty();
   },
 
-  renderMessage: function(message) {
-    var $userMsg = $('<div/>').append($('<div class="username"/>').text(message.username)).append ($('<div class="msg"/>').text(message.text));
+  renderMessage: message => {
+    var $userMsg = $('<div/>').addClass('chat').append($('<div class="username"/>').text(message.username)).append ($('<div class="msg"/>').text(message.text));
     // $('#chats').append($user);
 
     $('#chats').append( $userMsg );
   },
 
-  renderRoom: function(roomName) {
-    $('#roomSelect').append('<option value="' + roomName + '">' + roomName + '</option>');
+  renderRoom: message => {
+    var $roomName = $('<option/>').attr('value', message.roomname).text(message.roomname);
+    $('#roomSelect').append($roomName);
   },
 
-  handleUsernameClick: function() {
+  filterMessageByRoom: (roomname, message) =>{
+    //only render room when the roomname is the selected room
+    //if no room is specified, display all message
+    console.log('roomname',roomname);
+    if (message.roomname === roomname) {
+      console.log('before rendering');
+      app.renderMessage(message);
+    } else if (roomname === '') {
+      console.log('display all room');
+      app.renderMessage(message);
+    }
 
   },
 
-  handleSubmit: function() {
-    // console.log('at handleSubmit');
+  handleUsernameClick: () => {
+
+  },
+
+  handleSubmit: event => {
+    // event.preventDefault();
+    console.log('My sent');
+    var message = {
+      username: window.location.search.split('username=')[1],
+      text: $('#message').val(),
+      roomname: 'HackerAtReactor',
+
+    };
+    if (message.text !== '') {
+      console.log('My sent2');
+      app.send(message);
+    }
+    // event.preventDefault();
     // $('#send .submit').trigger('submit');
   },
 };
